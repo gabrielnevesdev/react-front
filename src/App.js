90650f86fa2c82142a54d0845270bed6a81bce76
gabriel neves <gabrielneves59.gn@gmail.com>
2022-03-07 17:00:02 -0300

    07/03/2022

--- src/App.js
+++ src/App.js
@@ -2,31 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
 import { PostCard } from './components/PostCard';
+import { loadPosts } from './utils/load-posts';
 
 class App extends Component{
-  state ={
+  state = {
     posts:[]
   };
 
-  componentDidMount(){
-    this.loadPosts()
+  async componentDidMount(){
+    await this.loadPosts()
   }
-  
-  loadPosts = async () => {
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
-    const photosResponse = fetch('https://jsonplaceholder.typicode.com/photos')
 
-    const [posts, photos] = await Promise.all([postsResponse, photosResponse])
-
-    const postsJson = await posts.json()
-    const photosJson = await photos.json()
-
-    const postsAndPhotos = postsJson.map((post, index)=>{
-      return {...post, cover: photosJson[index].url}
-    })
+  loadPosts = async()=>{
+    const postsAndPhotos = await loadPosts()
     this.setState({posts: postsAndPhotos})
   }
-
+  
   render(){
     const { posts } = this.state
 
